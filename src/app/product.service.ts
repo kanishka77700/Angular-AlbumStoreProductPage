f4c6ab15d09bbf851d2c97f2427e12d65e0480d4
kanishka77700 <kanishka77700@gmail.com>
2021-07-15 15:41:36 +0530

    Enter

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -22,7 +22,7 @@ return this._http.get(this._albumUrl).map((response) => <Album>(response.json())
 
   getProducts(): Observable<Product[]> {
 
-    return this._http.get(this._productsUrl).map(response => <Product[]> response.json());
+    return this._http.get(this._productsUrl).map(response => <Product[]> (response.json()));
   }
 
 }
